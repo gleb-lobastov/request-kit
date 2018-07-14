0b07f728c665ebf3e9d1bae814b2cbe23ec1dffe
gleb.lobastov <gleb.lobastov@onetwotrip.com>
2018-07-14 19:25:33 +0300

    build: add eslint and prettier to project
    Restrict and ensure code style for project
    
    Additional logic in .eslintrc.js is implemented to make possible for
    each package to detect it's own tests and sources paths and
    correspondingly apply "import/no-extraneous-dependencies" rule. I.e.
    each package could have own dependencies and devDependencies and
    mentioned rule should linting package files exactly in consideration of
    this package. In same time all test path patterns is shared across
    packages.
    
    "post-commit": "git update-index -g" in husky settings in
    package.json is added as workaround for jetbrains ide for reasons
    described here: https://www.npmjs.com/package/lint-staged#using-with-jetbrains-ides-webstorm-pycharm-intellij-idea-rubymine-etc

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const glob = require('glob');
+const lernaConfig = require('./lerna');
+
+const getPackageJsonPathsList = () =>
+  []
+    .concat(
+      ...lernaConfig.packages.map(packagePath =>
+        glob.sync(path.join(packagePath, 'package.json')),
+      ),
+    )
+    .map(packageConfigPath => packageConfigPath.replace('package.json', ''));
+
+const devDependenciesGlobs = [
+  '**/__tests__/**/test*.js',
+  '**/test/**/*.js',
+  '**/rollup.config.js',
+];
+
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+    'jest/globals': true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  parser: 'babel-eslint',
+  plugins: ['import', 'react', 'jest'],
+  root: true,
+  overrides: getPackageJsonPathsList().map(packageJsonPath => ({
+    files: [path.join(packageJsonPath, '..', '**/*.{js,jsx}')],
+    rules: {
+      'import/no-extraneous-dependencies': {
+        packageDir: `./${path.join(packageJsonPath, '..')}/`,
+        devDependencies: devDependenciesGlobs,
+      },
+    },
+  })),
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: './',
+        devDependencies: devDependenciesGlobs,
+      },
+    ],
+  },
+};
